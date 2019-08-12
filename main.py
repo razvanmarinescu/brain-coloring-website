64fcc60b16fced9615d4f4c22274126932eb5ad2
Razvan Marinescu <mrazvan22@gmail.com>
2019-08-12 18:32:07 -0400

    server working with gunicorn

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def allowed_file(filename):
   return '.' in filename and \
     filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/temp', methods=['GET', 'POST'])
 def upload_file():
   if request.method == 'POST':
     # check if the post request has the file part
@@ -95,8 +95,30 @@ def processFile(hash, fullFilePath):
 
 
 
-@app.route('/template/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      import random
+      hash = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+      filename = hash + '_' + secure_filename(file.filename)
+
+      fullFilePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+      # fullFilePath = '%s.csv' % (fullFilePath[:-4])
+
+      file.save(fullFilePath)
+
+      processFile(hash, fullFilePath)
     return render_template('index.html')
 
 
